b1a44523a31f6a62dd74b5ea45f2a8c53fde8548
co2generator <spacecraftstationa@163.com>
2021-07-19 14:42:23 +0800

    add 00-heap

---
+++ 00-堆.cpp
@@ -0,0 +1,85 @@
+/**
+ * ZJU Data Structure Note
+ * 
+ * Heap (堆)
+ * 
+ * 堆的特性：
+ *   （1）结构性：用数组表示的完全二叉树
+ *   （2）有序性：任一结点的关键字是其子树所有结点的最大值（或最小值），即最大堆、最小堆
+ */
+#include <iostream>
+using namespace std;
+#define MaxData 1000
+
+typedef int ElementType;
+typedef struct HeapStruct *MaxHeap;
+struct HeapStruct {
+    ElementType* data;     // 存储堆元素的数组
+    int size;              // 堆的当前元素个数
+    int capacity;          // 堆的最大容量
+};
+
+MaxHeap create(int maxSize);
+bool isEmpty(MaxHeap H);
+bool isFull(MaxHeap H);
+void insert(MaxHeap H, ElementType item);
+ElementType deleteMax(MaxHeap H);
+
+int main() {
+
+    return 0;
+}
+
+MaxHeap create(int maxSize) {
+    MaxHeap H = (MaxHeap)malloc(sizeof(struct HeapStruct));
+    H->data = (ElementType*)malloc((maxSize+1) * sizeof(ElementType));
+    H->size = 0;
+    H->capacity = maxSize;
+    H->data[0] = MaxData;
+    return H;
+}
+
+bool isFull(MaxHeap H) {
+    return H->size == H->capacity;
+}
+
+bool isEmpty(MaxHeap H) {
+    return H->size == 0;
+}
+
+void insert(MaxHeap H, ElementType item) {
+    int i;
+    if (isFull(H)) {
+        cout << "最大堆已满." << endl;
+        return;
+    }
+    i = ++H->size;
+    for ( ; H->data[i / 2] < item; i /= 2) {
+        H->data[i] = H->data[i / 2];
+    }
+    H->data[i] = item;
+}
+
+ElementType deleteMax(MaxHeap H) {
+    int parent, child;
+    ElementType maxItem, temp;
+    if (isEmpty(H)) {
+        cout << "最大堆已为空" << endl;
+        return;
+    }
+    maxItem = H->data[1];   // 取出根结点
+    temp = H->data[H->size--];
+    for (parent = 1; parent*2 <= H->size; parent=child) {
+        child = parent * 2;
+        if ((child != H->size) && H->data[child] < H->data[child+1]) {
+            child++;
+        }
+        if (temp >= H->data[child]) {
+            break;
+        } else {
+            H->data[parent] = H->data[child];
+        }
+    }
+    H->data[parent] = temp;
+    return maxItem;
+}
